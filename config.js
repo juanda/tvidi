d354e454bdfca06b0bb8d9711b2c427866e3ed6f
inaki76 <i.alberca.machado@gmail.com>
2014-10-19 16:28:46 +0200

    update config

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 var settings = {
-    apiUrl: "http://192.168.1.105:8000",
+    //apiUrl: "http://192.168.1.105:8000",
+    apiUrl: "http://localhost:8080/TvidiServer/web/app_dev.php",
     apiKey: 123456,
-    idSala: 6,
-    idHotel: 3,
-    // style puede ser: cielo, computers, gente, lomas, reunion, turquesa
-    style: 'computers'
+    idSala: 1,
+    idHotel: 1,
+    // style puede ser: lomas, cielo, gente, turquesa, reunion, computers
+    style: 'lomas'
 };
