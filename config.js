8fe9f2700dec6a2a718bd87c627433b9a922b86a
inaki76 <i.alberca.machado@gmail.com>
2014-10-20 16:40:29 +0200

    ajustes en el rest para mostrar solo los eventos del dia actual

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var settings = {
     //apiUrl: "http://192.168.1.105:8000",
-    apiUrl: "http://localhost:8080/TvidiServer/web/app_dev.php",
+    apiUrl: "http://192.168.1.33:8080/TvidiServer/web/app_dev.php",
     apiKey: 123456,
     idSala: 1,
     idHotel: 1,
 
-    // style puede ser: blanco, negrita, fractalia, cielo, computers, gente, lomas, reunion, turquesa, negro, aeropuerto y naranjas // 
+    // style puede ser: blanco, negrita, naranjas, negro, aeropuerto, fractalia, cielo, computers, gente, lomas, reunion, turquesa // 
     style: 'naranjas'
 };
