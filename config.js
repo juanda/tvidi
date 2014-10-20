9478312ee1e512b16635820e03e6db079022bafc
inaki76 <i.alberca.machado@gmail.com>
2014-10-20 22:59:12 +0200

    prueba

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 var settings = {
     //apiUrl: "http://192.168.1.105:8000",
-    apiUrl: "http://192.168.1.33:8080/TvidiServer/web/app_dev.php",
+    apiUrl: "http://localhost:8080/TvidiServer/web/app_dev.php",
+//    apiUrl: "http://178.33.1.85",
     apiKey: 123456,
     idSala: 1,
     idHotel: 1,
 
     // style puede ser: blanco, negrita, naranjas, negro, aeropuerto, fractalia, cielo, computers, gente, lomas, reunion, turquesa // 
-    style: 'naranjas'
+    style: 'fractalia'
 };
