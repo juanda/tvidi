8eec5900c24549fa8a8279e43b51abcd4b7dbabd
inaki76 <i.alberca.machado@gmail.com>
2014-11-01 22:54:26 +0100

    ajuste texto hotel

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 var settings = {
     //apiUrl: "http://192.168.1.105:8000",
-
-
 //    apiUrl: "http://localhost:8080/TvidiServer/web/app_dev.php",
     apiUrl: "http://178.33.1.85",
 
     apiKey: 123456,
     idSala: 1,
-    idHotel: 2,
+    idHotel: 1,
     timeRefresh: 1, // en minutos
     antelacion: 10000,  // en minutos
 
